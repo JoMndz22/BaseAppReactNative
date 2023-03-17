0d7a9b824620f8ec0091485fb74fd85873e4e996
Josue Mendez <josue.mendez087@gmail.com>
2023-03-17 11:59:38 -0600

    feat: remove imports

--- App.tsx
+++ App.tsx
@@ -1,100 +1,64 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+    SafeAreaView,
+    ScrollView,
+    StyleSheet,
+    useColorScheme,
+    View
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import { Row } from './src/components/atoms';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {Row} from './src/components/atoms';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+    const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    flex: 1,
-  };
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+        flex: 1
+    };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}
-        contentContainerStyle={styles.container}
-      >
-        <Row>
-          <View style={styles.box} />
-          <View style={styles.box} />
-        </Row>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={backgroundStyle}>
+            <ScrollView contentInsetAdjustmentBehavior="automatic"
+                style={backgroundStyle}
+                contentContainerStyle={styles.container}>
+                <Row>
+                    <View style={styles.box}/>
+                    <View style={styles.box}/>
+                    <View style={styles.box}/>
+                </Row>
+                <Row>
+                    <View style={styles.box}/>
+                </Row>
+                <Row>
+                    <View style={styles.box}/>
+                    <View style={styles.box}/>
+                </Row>
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    paddingHorizontal: 15,
-    columnGap: 15,
-  },
-  box:{
-    flexGrow: 1,
-    height: 100,
-    backgroundColor: 'white',
-    shadowColor: '#000',
-    shadowRadius: 10,
-    borderRadius: 10,
-    shadowOffset: {height:10, width:0},
-    shadowOpacity: 0.17
-  }
+    container: {
+        paddingHorizontal: 15,
+        rowGap: 15,
+        backgroundColor:'red'
+    },
+    box: {
+        flexGrow: 1,
+        height: 100,
+        backgroundColor: 'white',
+        shadowColor: '#000',
+        shadowRadius: 10,
+        borderRadius: 10,
+        shadowOffset: {
+            height: 10,
+            width: 0
+        },
+        shadowOpacity: 0.17
+    }
 });
 
 export default App;
