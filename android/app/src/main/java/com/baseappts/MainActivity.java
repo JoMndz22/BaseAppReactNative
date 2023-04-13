8d52222ae722e0e55d85c51bb046259ed37f02a4
Josue Mendez <josue.mendez087@gmail.com>
2023-04-13 10:53:27 -0600

    fix: add new splashscreen with anda logo

--- android/app/src/main/java/com/baseappts/MainActivity.java
+++ android/app/src/main/java/com/baseappts/MainActivity.java
@@ -1,5 +1,6 @@
 package com.baseappts;
 
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
